cc2dc34b14d71acce7c67bd9edf74ceb02291aab
marosmatej <114649931+marosmatej@users.noreply.github.com>
2022-11-19 22:46:34 +0100

    w3c

--- index.php
+++ index.php
@@ -1,80 +1,105 @@
-<!DOCTYPE html>
-<html lang="en">
+
+<!DOCTYPE HTML>  
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHPform</title>
-
-    <style>
-        *{
-            padding: 0;
-            margin: 0;
-            text-decoration: none;
-            list-style: none;
-            box-sizing: border-box;
-        }
-        main{
-            width: min-content;
-            min-width: 50%;
-            margin-left: auto;
-            margin-right: auto;
-            padding: 0.1vw 2vw 1vw 2vw;
-        }
-        form input{
-            padding: .5%;
-            margin: .4%;
-            font-family: 'Times New Roman', Times, serif;
-            font-size: 1.5vw;   
-        }
-        
-    </style>
+<style>
+.error {color: #FF0000;}
+</style>
 </head>
-<body>
-    <main>
-        <div class="form_wrap">
-            <h2>Login</h2>
-            <form action="">
-                <div>
-                <label for="">Username</label>
-                <input type="text" name="username" required>
-                </div>
-                <div>
-                <label for="">First Name</label>
-                <input type="text" name="firstname">
-                </div>
-                <div>
-                <label for="">Last Name</label>
-                <input type="text" name="lastname">
-                </div>
-                <div>
-                <label for="">Email</label>
-                <input type="email" name="email" required>
-                </div>
-                <div>
-                <label for="">Password</label>
-                <input type="password" name="password" required>
-                </div>
-                <div>
-                <label for="">Tel. number</label>
-                <input type="tel" name="tel">
-                </div>
+<body>  
 
-                <input type="submit" value="Save">
-            </form>
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
     
-        </div>
-    </main>
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
 </body>
 </html>
-<?php
-    ectract($_REQUEST)
-    $file=fopen("form_data.txt","a");
-
-    fwrite($file, $username . "\n");
-    fwrite($file, $firstname . "\n");
-    fwrite($file, $lastname . "\n");
-    fwrite($file, $email . "\n");
-    fwrite($file, $password . "\n");
-    fwrite($file, $tel . "\n");
-?>
