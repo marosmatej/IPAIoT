171ae6211b2d88b5908c5fdbd4a1fb53bc00a8cd
marosmatej <114649931+marosmatej@users.noreply.github.com>
2022-12-21 11:49:55 +0100

    try

--- index.php
+++ index.php
@@ -21,62 +21,24 @@
             margin-right: auto;
             padding: 0.1vw 2vw 1vw 2vw;
         }
-        form input{
-            padding: .5%;
-            margin: .4%;
-            font-family: 'Times New Roman', Times, serif;
-            font-size: 1.5vw;   
-        }
         
     </style>
 </head>
 <body>
     <main>
-        <div class="form_wrap">
-            <h2>Login2</h2>
-            <?php
-                if (isset($_POST['submit'])) {
-                    $file = fopen("form_data.txt", "a");
-                    fwrite($file, $_POST['username'] . "\n");
-                    fwrite($file, $_POST['firstname'] . "\n");
-                    fwrite($file, $_POST['lastname'] . "\n");
-                    fwrite($file, $_POST['email'] . "\n");
-                    fwrite($file, $_POST['password'] . "\n");
-                    fwrite($file, $_POST['tel'] . "\n");
-
-                    fclose($file);
-                }
-            ?>
-            <form action="" method="POST">
-                <div>
-                <label for="">Username</label>
-                <input type="text" name="username" required autocomplete="off">
-                </div>
-                <div>
-                <label for="">First Name</label>
-                <input type="text" name="firstname" autocomplete="off">
-                </div>
-                <div>
-                <label for="">Last Name</label>
-                <input type="text" name="lastname" autocomplete="off">
-                </div>
-                <div>
-                <label for="">Email</label>
-                <input type="email" name="email" required autocomplete="off">
-                </div>
-                <div>
-                <label for="">Password</label>
-                <input type="password" name="password" required autocomplete="off">
-                </div>
-                <div>
-                <label for="">Tel. number</label>
-                <input type="tel" name="tel" autocomplete="off">
-                </div>
-
-                <input type="submit" value="Save">
-            </form>
-    
-        </div>
+    <?php
+        echo '<h1>getParameters page</h1>';
+        
+        $sn1 = $_GET["a"];
+        $sn2 = $_GET["b"];
+        
+        $text = "a=" . $sn1 . " b=" . $sn2;
+        $sum = $sn1 + $sn2;
+        
+        echo $text;
+        echo "<br>";
+        echo "Sum:" . $sum;
+    ?>
     </main>
 </body>
 </html>
