49c2e7f3af249cd38cce5960c36e50353c1ca50c
marosmatej <114649931+marosmatej@users.noreply.github.com>
2022-12-21 17:48:17 +0100

    post

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
         echo '<h1>getParameters page</h1>';
         
-        $sn1 = $_GET["a"];
-        $sn2 = $_GET["b"];
+        $sn1 = $_POST["a"];
+        $sn2 = $_POST["b"];
         
         $text = "a=" . $sn1 . " b=" . $sn2;
         $sum = $sn1 + $sn2;
